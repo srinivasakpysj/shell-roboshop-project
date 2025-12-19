e23d6ddfa7f5bdc2be3e517beafe4ae1e0786acc
srinivasakpysj <srinivasakysj@gmail.com>
2025-12-19 07:31:54 +0530

    roboshop project

--- frontend.sh
+++ frontend.sh
@@ -5,7 +5,6 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
 LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
@@ -14,17 +13,19 @@ SCRIPT_DIR=$PWD
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-# Root check
-if [ $USERID -ne 0 ]; then
+# check the user has root priveleges or not
+if [ $USERID -ne 0 ]
+then
     echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1
+    exit 1 #give other than 0 upto 127
 else
     echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
-# Validate function
-VALIDATE() {
-    if [ $1 -eq 0 ]; then
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
         echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
     else
         echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
@@ -32,43 +33,34 @@ VALIDATE() {
     fi
 }
 
-# Disable default nginx
 dnf module disable nginx -y &>>$LOG_FILE
-VALIDATE $? "Disabling Default Nginx"
+VALIDATE $? "Disabling Default nginx"
 
-# Enable nginx 1.24
 dnf module enable nginx:1.24 -y &>>$LOG_FILE
-VALIDATE $? "Enabling Nginx:1.24"
+VALIDATE $? "Enabling nginx:1.24"
 
-# Install nginx
 dnf install nginx -y &>>$LOG_FILE
-VALIDATE $? "Installing Nginx"
+VALIDATE $? "Installing nginx"
 
-# Remove default content
+systemctl enable nginx  &>>$LOG_FILE
+systemctl start nginx 
+VALIDATE $? "Starting nginx"
+
 rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
 VALIDATE $? "Removing default content"
 
-# Download frontend
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading frontend"
 
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip &>>$LOG_FILE
-VALIDATE $? "Unzipping frontend"
+VALIDATE $? "unzipping frontend"
 
-# Replace nginx config BEFORE starting service
-rm -f /etc/nginx/nginx.conf &>>$LOG_FILE
-VALIDATE $? "Removing default nginx config"
+rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
+VALIDATE $? "Remove default nginx conf"
 
-cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>>$LOG_FILE
-VALIDATE $? "Copying custom nginx config"
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "Copying nginx.conf"
 
-# Validate nginx config
-nginx -t &>>$LOG_FILE
-VALIDATE $? "Validating nginx configuration"
-
-# Enable & start nginx
-systemctl daemon-reload &>>$LOG_FILE
-systemctl enable nginx &>>$LOG_FILE
-systemctl restart nginx &>>$LOG_FILE
-VALIDATE $? "Starting Nginx"
+systemctl restart nginx 
+VALIDATE $? "Restarting nginx"
